a708aaba82c705ff36be04601334d80812a6ed1a
Rahel Mahfooz Sarker <rahelsarker@gmail.com>
2019-10-31 00:57:27 +0600

    Added login debug

--- telnetCisco.py
+++ telnetCisco.py
@@ -2,13 +2,13 @@ import telnetlib
 USERNAME = "cisco"
 PASSWORD = "cisco"
 HOST = "10.0.0.10"
-print("USERNAME: ", USERNAME, "\nPASSWORD: ", PASSWORD)
+#print("USERNAME: ", USERNAME, "\nPASSWORD: ", PASSWORD)
 connectTelnet = telnetlib.Telnet(HOST)
-terminal = connectTelnet.read_until(b":")
+terminal = connectTelnet.read_until(":".encode('ascii'))
 
-if b"Username" in terminal:
+if "Username".encode('ascii') in terminal:
 	connectTelnet.write(USERNAME.encode('ascii') + "\n".encode('ascii') + PASSWORD.encode('ascii'))
-	terminal = connectTelnet.read_until(b":")
+	terminal = connectTelnet.read_until(">".encode('ascii'))
 	print("Login Success\n", terminal.decode("ascii"))
 else:
 	print(terminal.decode("ascii"))
