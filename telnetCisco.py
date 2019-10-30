2f50eadfadf8111727fd326338be02b32cc24bb4
Rahel Mahfooz Sarker <rahelsarker@gmail.com>
2019-10-31 00:39:34 +0600

    Added decoding

--- telnetCisco.py
+++ telnetCisco.py
@@ -5,4 +5,4 @@ HOST = "10.0.0.10"
 print("USERNAME: ", USERNAME, "\nPASSWORD: ", PASSWORD)
 connectTelnet = telnetlib.Telnet(HOST)
 terminal = connectTelnet.read_until(b"Username:")
-print(terminal)
+print(terminal.decode("ascii"))
