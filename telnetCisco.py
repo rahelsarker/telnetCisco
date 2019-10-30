74f4f84ee855419971335b46680fbaff2c1424e9
Rahel Mahfooz Sarker <rahelsarker@gmail.com>
2019-10-31 01:34:49 +0600

    Added configure terminal

--- telnetCisco.py
+++ telnetCisco.py
@@ -4,22 +4,22 @@ PASSWORD = "cisco"
 HOST = "10.0.0.10"
 #print("USERNAME: ", USERNAME, "\nPASSWORD: ", PASSWORD)
 connectTelnet = telnetlib.Telnet(HOST)
-terminal = connectTelnet.read_until(":".encode('ascii'))
+terminal = connectTelnet.read_until(":".encode('ascii'), 2)
 
 if b"Username" in terminal:
 	connectTelnet.write(USERNAME.encode('ascii') + "\n".encode('ascii'))
-	terminal = connectTelnet.read_until(b":")
+	terminal = connectTelnet.read_until(b":", 2)
 	connectTelnet.write(PASSWORD.encode('ascii') + "\n".encode('ascii'))
-	terminal = connectTelnet.read_until(b">")
+	terminal = connectTelnet.read_until(b">", 2)
 	print("Login Success\n", terminal.decode("ascii"))
 	connectTelnet.write(b"enable" + b"\n")
-	terminal = connectTelnet.read_all()
+	terminal = connectTelnet.read_all(2)
 	if b"Password:" in terminal:
 		connectTelnet.write(PASSWORD.encode('ascii') + "\n".encode('ascii'))
 	else:
 		print(terminal.decode("ascii"))
 	connectTelnet.write(b"configure terminal" + b"\n")
-	terminal = connectTelnet.read_until(b"#")
+	terminal = connectTelnet.read_until(b"#", 2)
 	print(terminal.decode("ascii"))
 else:
 	print(terminal.decode("ascii"))
